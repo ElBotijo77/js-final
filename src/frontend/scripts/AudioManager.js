8c79737758377828be534ae6321c6a98363928cd
Erik <nikolaibellics@gmail.com>
2026-05-14 12:40:06 +0200

    chore: final adjustments for correct functionality

--- src/frontend/scripts/AudioManager.js
+++ src/frontend/scripts/AudioManager.js
@@ -1,48 +1,64 @@
 ﻿class AudioManager {
   constructor() {
-    this.click = new Audio("./assets/audio/sounds/click.mp3");
-    this.check = new Audio("./assets/audio/sounds/check.mp3");
-    this.correct = new Audio("./assets/audio/sounds/correct.mp3");
-    this.hover = new Audio("./assets/audio/sounds/hover.mp3");
-    this.music = new Audio("./assets/audio/music/music.mp3");
 
+    // Importamos la musisca de fondo desde el HTML para guardalo en una variable
+    this.correct = new Audio("./assets/audio/sounds/correct.mp3");
+    this.wrong = new Audio("./assets/audio/sounds/wrong.mp3");
+    this.music = new Audio("./assets/audio/music/music.mp3");
+    this.hover = new Audio("./assets/audio/sounds/hover.mp3");
+
+    // Establecemos que se ponga en Loop, previamente modificada en Ableton para que no se note 
+    // la bajada de audio final. Se establece un volumen de 0.5 para que este presente
     this.music.loop = true;
     this.music.volume = 0.5;
 
-    this.click.load();
-    this.check.load();
+    // Para evitar un pequeño delay inicial, cargarmos los sonidos en la caché
     this.correct.load();
+    this.wrong.load();
     this.hover.load();
     this.music.load();
 
+    // Guardamos el estado de musica activada
     this.musicEnabled = localStorage.getItem("musicEnabled") !== "false";
-
-    if (this.musicEnabled) {
-      this.music.play().catch(() => {
-        // El navegador puede bloquear la reproducción automática hasta que el usuario interactúe.
-      });
-    }
   }
 
+
+  // Con cloneNode evitamos cortes al pulsar varias veces segudas el mismo boton
+  // Bajamos un poco el volumen de wrong, ya que no se ha masterizado
   playEffect(sound) {
     const copySound = sound.cloneNode();
-    copySound.volume = 0.5;
+    copySound.volume = 1;
+    if(sound === this.wrong) copySound.volume = 0.5;
     copySound.play();
   }
 
+
+  // Alterna muscia en apagado/encendido. Se guarda el estado en localStorage
   toggleMusic() {
     this.musicEnabled = !this.musicEnabled;
     localStorage.setItem("musicEnabled", this.musicEnabled);
 
-    if (this.musicEnabled) {
-      this.music.play().catch(() => {
-        // El navegador puede bloquear la reproducción automática hasta que el usuario interactúe.
-      });
-    } else {
-      this.music.pause();
+    // Inicia o detiene la muscia según el estado
+    if (this.musicEnabled) this.music.play()
+    else this.music.pause();
     }
+  
+
+  // Creamos un metodo que ejecute el sonido Hover cuando el raton entra dentro del boton
+  addHoverToClass(className, sound = this.hover) {
+      const buttons = document.querySelectorAll(`.${className}`);
+
+      console.log("Botones encontrados:", buttons.length);
+
+      buttons.forEach(button => {
+          button.addEventListener("mouseenter", () => {
+              this.playEffect(sound);
+          });
+      });
   }
 
+  // Actualizar la interfaz visual del botón de música para que coincida con el estado actual del juego
+  // tachandolo con una raya la nota muscial(Icono)
   updateButton(button) {
     if (!button) return;
 
