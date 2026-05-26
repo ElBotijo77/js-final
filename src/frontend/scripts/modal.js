47a4b76b97f761599f471935f0135ee02693edac
Pepitero <nikolaibellics@gmail.com>
2026-05-26 08:04:54 +0200

    feat: implement only UI form

---
+++ src/frontend/scripts/modal.js
@@ -0,0 +1,7 @@
+function modalBox() {
+    const modal = document.querySelector('.modal');
+    const openModalBtn = document.querySelector('.boton-menu.ranking-menu');
+    const closeModalBtn = document.querySelector('.close-modal-btn');document.getElementById("settingsBtn")
+    .addEventListener("click", () => {
+        // Logica para abrir el modal
+})}; 
